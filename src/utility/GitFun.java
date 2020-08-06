5650fc0145ab32bc011e9989e9f742e016f09a0b
catzinwest <68832716+HakkiSabanci@users.noreply.github.com>
2020-08-05 21:32:56 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,5 +3,7 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Java and Git is funnnn");
+       //This line is from GitHub
+        System.out.println("GitHub")
     }
 }
