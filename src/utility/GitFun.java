8672473b40c326703fbc3d2cd5c4dffdcddb1cbe
HakkiSabanci <hakkisabanci@yahoo.com>
2020-08-05 21:34:05 -0400

    update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -4,6 +4,6 @@ public class GitFun {
     public static void main(String[] args) {
         System.out.println("Java and Git is funnnn");
        //This line is from GitHub
-        System.out.println("GitHub")
+        System.out.println("GitHub");
     }
 }
